f0dda5c4c5a378326cd5ee9ee6ebafff4e3a809e
emitaylor <letailleur.emilie@gmail.com>
2024-08-17 13:04:08 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -61,10 +61,22 @@ const App = () => {
             }}
           />
       </motion.div>
-
+      {/* Button */}
       <motion.div
         variants={ gridSquareVariants }
         className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10">
+           <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{
+              scale: 1.1,
+              backgroundColor: "#d1d5db",
+              color: "black",
+            }}
+            transition={{ bounceDamping: 10, bounceStiffness: 600 }}
+            className="bg-emerald-600 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
+          >
+            subscribe
+          </motion.button>
       </motion.div>
 
       <motion.div
